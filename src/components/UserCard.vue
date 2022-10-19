a6a50a18000049e0837d661220ca4e442022943d
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-19 21:25:42 +0300

    user card component ... wip

---
+++ src/components/UserCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-card">
+    <img src="">
+    <h6></h6>
+  </div>
+</template>
+
+<style scoped>
+.user-card img{
+  width: 100%;
+  height: 240px;
+  border-radius: 4px;
+  object-fit: cover;
+}
+.user-card h6{
+  font-size: 20px;
+  margin-top: 8px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-2">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
